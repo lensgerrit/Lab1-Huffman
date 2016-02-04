f37e6d9d2d92fd6ae8d8e30d2d89d1851b538e69
lensgerrit <gerrit@lensinkeyesurgery.com>
2016-02-03 18:30:11 -0700

    Create PriorityQApp.java

---
+++ PriorityQApp.java
@@ -0,0 +1,22 @@
+
+import java.io.IOException;
+
+class PriorityQApp
+{
+	public static void main(String[] args) throws IOException
+	{
+		PriorityQ thePQ = new PriorityQ(5);
+//		thePQ.insert(30);
+//		thePQ.insert(50);
+//		thePQ.insert(10);
+//		thePQ.insert(40);
+//		thePQ.insert(20);
+		while( !thePQ.isEmpty() )
+		{
+			Tree item = thePQ.remove();
+			System.out.print(item + " "); // 10, 20, 30, 40, 50
+		} // end while
+		System.out.println("");
+	} // end main()
+	//-------------------------------------------------------------
+} // end clas
